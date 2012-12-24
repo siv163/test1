f3a23a5876cfa253c847201d4942fa4d343e6ce0
igoryxa <igoryxa@compik.WORKNET>
2012-12-24 21:35:20 +0400

    install commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = '466d99d498f0f9b351e49b3a399c5603ef6c531c25209a483dc1981323025325b6775ca0b92c659ba5cda679892ca18a76cf1eb4d6c10b327c7057835c55d204'
